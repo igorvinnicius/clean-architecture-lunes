21f29725349e7a0a082fcaca50f9006a7347ce74
Igor Vinícius <igorvinnicius@gmail.com>
2018-09-02 16:22:56 +0200

    creating an account (first test)

---
+++ src/lunes.Domain/Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lunes.Domain.Entities
+{
+    public class Account
+    {
+	    public Guid Id { get; }
+	    public string Name { get; private set; }
+	    public double Balance { get; private set; }
+    }
+}
---
+++ tests/lunes.Domain.UnitTests/Accounts/WhenAnAccountIsCreated.cs
@@ -0,0 +1,18 @@
+﻿using lunes.Domain.Entities;
+using Xunit;
+
+namespace lunes.Domain.UnitTests.Accounts
+{
+    public class WhenAnAccountIsCreated
+    {
+	    [Fact]
+	    public void ShoudHaveACurrentBalanceZero()
+	    {
+		    var sut = new Account();
+
+			Assert.Equal(0, sut.Balance);
+	    }
+
+
+    }
+}
