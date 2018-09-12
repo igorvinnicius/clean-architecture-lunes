5a62990b08ffe6d8cb4fd9b6959ab918f7e574bd
Igor Vinícius <igorvinnicius@gmail.com>
2018-09-12 07:00:31 +0200

    when creating a revenue ShouldInitializeCorretly

---
+++ tests/lunes.Domain.UnitTests/Accounts/WhenCreatingARevenue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using lunes.Domain.Accounts;
+using Xunit;
+
+namespace lunes.Domain.UnitTests.Accounts
+{
+    public class WhenCreatingARevenue
+    {
+	    [Fact]
+	    public void ShouldInitializeCorretly()
+	    {
+		    var expectedName = "Name";
+		    var expectedAmount = 100;
+		    var expectedDate = DateTime.Now;
+
+		    var sut = new Revenue(expectedName, expectedAmount);
+
+		    Assert.Equal(expectedName, sut.Name);
+		    Assert.Equal(expectedAmount, sut.Amount);
+		    Assert.Equal(expectedDate.Date, sut.Date.Date);
+
+	    }
+	}
+}
