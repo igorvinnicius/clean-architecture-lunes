2422aa53522fdbe45c08cc150c40971af3ff0a95
Igor Vinícius <igorvinnicius@gmail.com>
2018-09-14 07:28:19 +0200

    added lunes.Application project; added CreateAccountUseCase

---
+++ src/lunes.Application/UseCases/Account/CreateAccount/CreateAccountUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lunes.Application.UseCases.Account.CreateAccount
+{
+    public class CreateAccountUseCase
+    {
+    }
+}
--- tests/lunes.UseCases.UnitTests/Account/WhenCreatingAnAccount.cs
+++ tests/lunes.UseCases.UnitTests/Account/WhenCreatingAnAccount.cs
@@ -1,6 +1,20 @@
-﻿namespace lunes.UseCases.UnitTests.Account
+﻿using lunes.Application.UseCases.Account.CreateAccount;
+using Xunit;
+
+namespace lunes.UseCases.UnitTests.Account
 {
     public class WhenCreatingAnAccount
     {
+	    [Fact]
+	    public void ShouldHasANameAndBalanceZero()
+	    {
+		    var expectedAccountName = "Use Case Account";
+		    var expectedBalance = 0;
+
+		    var sut = new CreateAccountUseCase();
+
+
+	    }
+
     }
 }
