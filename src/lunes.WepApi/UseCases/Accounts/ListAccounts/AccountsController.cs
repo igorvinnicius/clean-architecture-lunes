c42f7422507b7fcd07f885944f2883d4a4faf372
Igor Vinícius <igorvinnicius@gmail.com>
2019-02-27 20:59:13 +0100

    [WebApi] added AccountsController to list accounts

---
+++ src/lunes.WepApi/UseCases/Accounts/ListAccounts/AccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lunes.WepApi.UseCases.Accounts.ListAccounts
+{
+	[Route("api/[controller]")]
+	public class AccountsController : Controller
+    {
+
+    }
+}
