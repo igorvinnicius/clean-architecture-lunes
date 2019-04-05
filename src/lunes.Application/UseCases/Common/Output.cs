2ea52d302e9fbb1e217184dcea845c0acb944d84
Igor Vinícius <igorvinnicius@gmail.com>
2019-04-05 20:08:23 +0200

    removing unecessary code

--- src/lunes.Application/UseCases/Common/Output.cs
+++ src/lunes.Application/UseCases/Common/Output.cs
@@ -5,19 +5,7 @@ namespace lunes.Application.UseCases.Common
 {
     public class Output
     {
-	    protected List<string> Errors { get; set; } = new List<string>();
-
-	    protected bool HasErrors  => Errors.Any();
-
-	    protected void AddError(string error)
-	    {
-			Errors.Add(error);
-	    }
-
-	    protected void AddErrors(List<string> errors)
-	    {
-		    Errors.AddRange(errors);
-	    }
+	   
 
 	}
 }
